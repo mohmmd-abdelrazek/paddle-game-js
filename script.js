e292d64268a9f901a42b309dcb4216375b73819f
Mohamed <mohmmd.abdelrazek@gmail.com>
2023-11-26 20:31:59 +0200

    finish the game

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
 const container = document.getElementById("game-container"),
-board = document.getElementById("board"),
 ball = document.getElementById("ball"),
+board = document.getElementById("board"),
 gameOverScreen = document.getElementById("game-over"),
 startPage = document.getElementById("start-page"),
 scoreElement = document.getElementById("score"),
-levelElement = document.getElementById("level")
+levelElement = document.getElementById("level");
 
-let level = localStorage.getItem("level") || 1,
-boardSpeed= 10,
-ballSpeedX= 8 + +level,
-ballSpeedY= 8 + +level,
-boardX= container.offsetWidth / 2 - board.offsetWidth / 2,
-ballX= container.offsetWidth / 2 - ball.offsetWidth / 2,
-ballY= container.offsetHeight - board.offsetHeight - ball.offsetHeight - 20,
-score = 0,
-isLeftArrowPressed = false,
-isRightArrowPressed = false,
-isGameRunning = false,
-isGamePaused = false,
-animationFrameId;
+let level = localStorage.getItem("level") || 1;
+let boardSpeed, ballSpeedX, ballSpeedY, boardX, ballX, ballY, newSpeed;
+let score = 0, boardWidth, isLeftArrowPressed = false, isRightArrowPressed = false, isGameRunning = false, isGamePaused = false, animationFrameId;
 
 levelElement.innerHTML = level;
+setSpeed();
+setPosition();
 
 function move() {
     if (!isGameRunning || isGamePaused) return;
 
-    isLeftArrowPressed && board.offsetLeft > 0 && (boardX -= boardSpeed);
-    isRightArrowPressed && board.offsetLeft < (container.offsetWidth - board.offsetWidth) && (boardX += boardSpeed);
-    board.style.left = boardX + "px";
+    isLeftArrowPressed && (document.getElementById("board")).offsetLeft > 0 && (boardX -= boardSpeed);
+    isRightArrowPressed && (document.getElementById("board")).offsetLeft < (container.offsetWidth - (document.getElementById("board")).offsetWidth) && (boardX += boardSpeed);
+    (document.getElementById("board")).style.left = boardX + "px";
 
     ballX += ballSpeedX;
     ballY -= ballSpeedY;
@@ -39,21 +31,14 @@ function move() {
         ballSpeedX *= -1;
     }
 
-    if (board.offsetTop - ball.offsetHeight == ball.offsetTop) {
-        if (leftBoardCollision() || rightBoardCollision() || centerBoardCollision()) {
+    if ((document.getElementById("board")).offsetTop - ball.offsetHeight == ball.offsetTop && boardCollision()) {
             ballSpeedY *= -1;
             score++;
             scoreElement.innerHTML = score;
-            if (leftBoardCollision() && ballSpeedX > 0) {
-                ballSpeedX *= -1;
-            } else if (rightBoardCollision() && ballSpeedX < 0) {
-                ballSpeedX *= -1;
-            }
         }
-        
-    }
     
     if (score == 5 && level < 10) {
+        newSpeed = ballSpeedX;
         finishLevel();
     }
 
@@ -61,9 +46,25 @@ function move() {
         gameOver();
     }
     
-
-    
     animationFrameId = requestAnimationFrame(move);
+}
+
+function setSpeed() {
+    boardSpeed= 14;
+    ballSpeedX= 10 + +level / 1.25;
+    ballSpeedY= 15 + +level / 1.25;
+    
+}
+
+function setPosition() {
+    boardWidth= container.offsetWidth * 0.25 - +level * 3;
+    (document.getElementById("board")).style.width = boardWidth + "px";
+    boardX= container.offsetWidth / 2 - (document.getElementById("board")).offsetWidth / 2;
+    (document.getElementById("board")).style.left = boardX + "px";
+    ballX= container.offsetWidth / 2 - ball.offsetWidth / 2;
+    ball.style.left = ballX + "px";
+    ballY= container.offsetHeight - (document.getElementById("board")).offsetHeight - ball.offsetHeight - 20;
+    ball.style.top = ballY + "px";
 }
 
 function startGame() {
@@ -73,63 +74,51 @@ function startGame() {
         levelElement.innerHTML = level;
     }
     isGameRunning = true;
-    boardSpeed = 10;
-    ballSpeedX= 8 + +level;
-    ballSpeedY= 8 + +level;
-    boardX = container.offsetWidth / 2 - board.offsetWidth / 2;
-    ballX = container.offsetWidth / 2 - ball.offsetWidth / 2;
-    ballY = container.offsetHeight - board.offsetHeight - ball.offsetHeight - 20;
-    document.getElementById("pause-button").style.display = "block";
+    setSpeed();
+    setPosition();  
+    level % 2 == 0 && (ballSpeedX *= -1);
+    gameOverScreen.style.display = "none";
+    document.getElementById("pause-button").style.visibility = "visible";
     startPage.style.display = "none";
     score = 0;
     scoreElement.innerHTML = score;
     animationFrameId = requestAnimationFrame(move);
 }
 
-function leftBoardCollision() {
-    return board.offsetLeft - ball.offsetWidth <= ball.offsetLeft &&
-    board.offsetLeft + board.offsetWidth / 2 > ball.offsetLeft + ball.offsetWidth / 2;
+function boardCollision() {
+    return (document.getElementById("board")).offsetLeft - ball.offsetWidth < ball.offsetLeft &&
+    (document.getElementById("board")).offsetLeft + (document.getElementById("board")).offsetWidth > ball.offsetLeft;
 }
-function rightBoardCollision() {
-    return board.offsetLeft + board.offsetWidth / 2 < ball.offsetLeft + ball.offsetWidth / 2 &&
-    board.offsetLeft + board.offsetWidth >= ball.offsetLeft;
-}
-function centerBoardCollision() {
-    return board.offsetLeft + board.offsetWidth / 2 == ball.offsetLeft + ball.offsetWidth / 2;
-    
-}
-
 
 function pauseGame() {
     isGamePaused = !isGamePaused;
-    document.getElementById("pause-button").textContent = isGamePaused ? "&#9658;" : "||";
+    document.getElementById("pause-button").innerHTML = isGamePaused ? "&#9658;" : "||";
     isGamePaused ? cancelAnimationFrame(animationFrameId) : animationFrameId = requestAnimationFrame(move);
 }
 
 function gameOver() {
     document.querySelector(".game-over__text").innerHTML = "Game over!";
+    document.querySelector(".game-over__text").style.color = "red";
     isGameRunning = false;
     cancelAnimationFrame(animationFrameId);
     gameOverScreen.style.display = "flex";
-    document.getElementById("pause-button").style.display = "none";
+    document.getElementById("pause-button").style.visibility = "hidden";
     if (level == 10 && score == 5) {
-      document.querySelector(".game-over__text").innerHTML = "You win!";
+        document.querySelector(".game-over__text").innerHTML = "You win!";
+        document.querySelector(".game-over__text").style.color = "green";
     }    
 }
 
 function finishLevel() {
     level++;
     localStorage.setItem("level", level);
-    levelElement.innerHTML = level; 
-    ballSpeedX > 0 ? ballSpeedX = 8 + +level : ballSpeedX = -8 - +level;
-    ballSpeedY > 0 ? ballSpeedY = 8 + +level : ballSpeedY = -8 - +level;
+    levelElement.innerHTML = level;
+    setSpeed();
+    ballSpeedX = newSpeed;
+    boardWidth= container.offsetWidth * 0.25 - +level * 3;
+    (document.getElementById("board")).style.width = boardWidth + "px";
     score = 0;
     scoreElement.innerHTML = score;
-}
-
-function resetGame() {
-    gameOverScreen.style.display = "none";
-    startGame();
 }
 
 function cancelGame() {
@@ -140,6 +129,7 @@ function cancelGame() {
     level == 10 && (level = 1);
     localStorage.setItem("level", level);
     levelElement.innerHTML = level;
+    setPosition();
 }
 
 function handleKey(event, isDown) {
@@ -147,13 +137,16 @@ function handleKey(event, isDown) {
     if (event.key === "ArrowRight") isRightArrowPressed = isDown;
 }
 
+function handleSpaceKey () {
+    !isGameRunning ? startGame() : pauseGame();
+}
+
 document.addEventListener("keydown", (e) => handleKey(e, true));
 document.addEventListener("keyup", (e) => handleKey(e, false));
-
-
+document.addEventListener("keydown", (e) => e.key === " "  && handleSpaceKey());
+document.addEventListener("keydown", (e) => (e.key === "Escape" || e.key === "Esc") && cancelGame());
+window.addEventListener("resize", setPosition);
 document.getElementById("start-button").addEventListener("click", startGame);
+document.getElementById("restart-button").addEventListener("click", startGame);
 document.getElementById("pause-button").addEventListener("click", pauseGame);
-document.getElementById("restart-button").addEventListener("click", resetGame);
 document.getElementById("cancel-button").addEventListener("click", cancelGame);
-
-
